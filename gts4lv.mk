e9813a22d604d1610785b108ffa267e6d369f256
Erfan Abdi <erfangplus@gmail.com>
2022-08-04 01:48:15 +0430

    Adapt to LMODroid by device-add script
    Change-Id: Ic373cf339febf2d1f21bfb1297f8683758f02fd2

--- gts4lv.mk
+++ gts4lv.mk
@@ -35,7 +35,7 @@ TARGET_SCREEN_WIDTH := 1600
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-lmodroid
 
 PRODUCT_ENFORCE_RRO_TARGETS += *
 
